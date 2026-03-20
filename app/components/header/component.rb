19a1434f3ef614528367bb889d3a422a7d067459
Georg Ledermann <georg@ledermann.dev>
2026-03-20 08:14:45 +0100

    Navbar

--- app/components/header/component.rb
+++ app/components/header/component.rb
@@ -1,4 +1,33 @@
 module Header
   class Component < ViewComponent::Base
+    TAB_DEFINITIONS = [
+      { id: :configuration, path_helper: :configuration_path, icon: 'fa-solid fa-wrench' },
+      { id: :generated_files, path_helper: :generated_files_path, icon: 'fa-solid fa-file-code', expert_only: true },
+      { id: :services, path_helper: :services_path, icon: 'fa-solid fa-server' },
+    ].freeze
+
+    delegate :preferences, to: :helpers
+
+    def initialize(active_tab:)
+      super()
+      @active_tab = active_tab
+    end
+
+    private
+
+    attr_reader :active_tab
+
+    def tabs
+      TAB_DEFINITIONS
+        .reject { |tab| tab[:expert_only] && !preferences.expert_mode? }
+        .map { |tab| tab.merge(path: send(tab[:path_helper]), label: t(".#{tab[:id]}")) }
+    end
+
+    def tab_classes(tab)
+      class_names(
+        'inline-flex items-center gap-1 p-2 text-sm',
+        'border-b-2 border-current': tab[:id] == active_tab,
+      )
+    end
   end
 end
